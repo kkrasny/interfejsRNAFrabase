12f5aed7624011352d82cc0bc79164b113bc029c
Kamila <krasny.kamila@gmail.com>
2017-02-06 19:25:42 +0100

    all the activities. menu

--- app/src/main/java/com/example/user/interfejsrnafrabase/AdvancedSearchActivity.java
+++ app/src/main/java/com/example/user/interfejsrnafrabase/AdvancedSearchActivity.java
@@ -1,13 +1,18 @@
 package com.example.user.interfejsrnafrabase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AdvancedSearchActivity extends AppCompatActivity {
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,70 @@ public class AdvancedSearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_residue:
+                i = new Intent(getApplicationContext(), Residue.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_base:
+                i = new Intent(getApplicationContext(), Basepair.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_multi:
+                i = new Intent(getApplicationContext(), Multiplet.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_dinucl:
+                i = new Intent(getApplicationContext(), DinuclStep.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_stem:
+                i = new Intent(getApplicationContext(), Stem.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_loop:
+                i = new Intent(getApplicationContext(), Loop.class);
+                startActivity(i);
+                return true;
+            case R.id.menu_secondary:
+                Intent ijj = new Intent(getApplicationContext(), SecondaryStruct.class);
+                startActivity(ijj);
+                return true;
+            case R.id.menu_help:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                return true;
+            case R.id.menu_about:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                return true;
+            case R.id.menu_contact:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                return true;
+            case R.id.menu_links:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
--- app/src/main/java/com/example/user/interfejsrnafrabase/MainActivity.java
+++ app/src/main/java/com/example/user/interfejsrnafrabase/MainActivity.java
@@ -2,38 +2,41 @@
 package com.example.user.interfejsrnafrabase;
 
 import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     // URL Address
     String url = "http://rnafrabase.cs.put.poznan.pl/";
     ProgressDialog mProgressDialog;
+    Intent i;
+    String value;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         // Locate the Buttons in activity_main.xml
         Button searchbutton = (Button) findViewById(R.id.search_button);
         Button choosebutton = (Button) findViewById(R.id.choose_button);
@@ -59,11 +62,81 @@ public class MainActivity extends Activity {
         advsearchbutton.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
                 // Execute Logo AsyncTask
-                new AdvSearch().execute();
+                Intent i = new Intent(getApplicationContext(), AdvancedSearchActivity.class);
+                startActivity(i);
             }
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_residue:
+                i = new Intent(getApplicationContext(), Residue.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_base:
+                i = new Intent(getApplicationContext(), Basepair.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_multi:
+                i = new Intent(getApplicationContext(), Multiplet.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_dinucl:
+                i = new Intent(getApplicationContext(), DinuclStep.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_stem:
+                i = new Intent(getApplicationContext(), Stem.class);
+                startActivity(i);
+                return true;
+            case R.id.menu2_loop:
+                i = new Intent(getApplicationContext(), Loop.class);
+                startActivity(i);
+                return true;
+            case R.id.menu_secondary:
+                i = new Intent(getApplicationContext(), SecondaryStruct.class);
+                startActivity(i);
+                return true;
+            case R.id.menu_help:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                i.putExtra(value, "Help");
+                return true;
+            case R.id.menu_about:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                i.putExtra(value, "About");
+                return true;
+            case R.id.menu_contact:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                i.putExtra(value, "Contact");
+                return true;
+            case R.id.menu_links:
+                i = new Intent(getApplicationContext(), New.class);
+                startActivity(i);
+                i.putExtra(value, "Links");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     // Title AsyncTask
     private class ChooseFile extends AsyncTask<Void, Void, Void> {
@@ -112,7 +185,7 @@ public class MainActivity extends Activity {
                 Document document = Jsoup.connect(url).get();
                 // Using Elements to get the Meta data
                 Element search = document.getElementById("send");
-             //           .select("meta[name=description]");
+                //           .select("meta[name=description]");
                 // Locate the content attribute
                 //desc =
                 search.attr("content");
@@ -144,9 +217,9 @@ public class MainActivity extends Activity {
 
 //        @Override
 //        protected void onPostExecute(Void result) {
-            // Set downloaded image into ImageView
+        // Set downloaded image into ImageView
 //            ImageView logoimg = (ImageView) findViewById(R.id.logo);
 //            logoimg.setImageBitmap(bitmap);
 //            mProgressDialog.dismiss();
-        }
     }
+}
